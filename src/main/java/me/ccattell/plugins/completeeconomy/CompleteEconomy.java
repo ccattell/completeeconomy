0fd38125bdec5ddaedeacb6946fcb856212da0fb
Charles Cattell <ccattell@mymsmtech.com>
2013-04-18 18:37:19 -0500

    CE

---
+++ src/main/java/me/ccattell/plugins/completeeconomy/CompleteEconomy.java
@@ -0,0 +1,25 @@
+package me.ccattell.plugins.completeeconomy;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CompleteEconomy extends JavaPlugin implements Listener {
+
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+    }
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome, Grandmaster " + event.getPlayer().getDisplayName() + "!");
+    }
+}
+
