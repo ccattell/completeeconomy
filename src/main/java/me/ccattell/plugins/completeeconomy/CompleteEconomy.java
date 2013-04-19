cb0a27a3b7cfb168fadcf9ce3b5becd660b1c180
eccentricdevotion <eccentricdevotion@gmail.com>
2013-04-19 18:58:26 +1200

    Update CompleteEconomy.java
    Add java.io.File import

--- src/main/java/me/ccattell/plugins/completeeconomy/CompleteEconomy.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/CompleteEconomy.java
@@ -1,5 +1,6 @@
 package me.ccattell.plugins.completeeconomy;
 
+import java.io.File;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,31 +11,36 @@ import org.bukkit.plugin.java.JavaPlugin;
 import me.ccattell.plugins.completeeconomy.database.CEDatabase;
 
 public class CompleteEconomy extends JavaPlugin implements Listener {
+
     public static CompleteEconomy plugin;
     CEDatabase service = CEDatabase.getInstance();
     public PluginDescriptionFile pdfFile;
     public ConsoleCommandSender console;
     public String pluginName;
-	public void onDisable() {
-		// TODO: Place any custom disable code here.
-	}
-	public void onEnable() {
-            pdfFile = getDescription();
-            pluginName = ChatColor.GOLD + "[" + pdfFile.getName() + "]" + ChatColor.RESET + " ";
-            plugin = this;
-            console = getServer().getConsoleSender();
-            try {
-                String path = getDataFolder() + File.separator + "CompleteEconomy.db";
-                    service.setConnection(path);
-                    service.createTables();
-            } catch (Exception e) {
-                console.sendMessage(pluginName + "Connection and Tables Error: " + e);
-            }
-            getServer().getPluginManager().registerEvents(this, this);
-	}
 
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		event.getPlayer().sendMessage("Welcome, Grandmaster " + event.getPlayer().getDisplayName() + "!");
-	}
-} 
+    @Override
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+    }
+
+    @Override
+    public void onEnable() {
+        pdfFile = getDescription();
+        pluginName = ChatColor.GOLD + "[" + pdfFile.getName() + "]" + ChatColor.RESET + " ";
+        plugin = this;
+        console = getServer().getConsoleSender();
+        try {
+            String path = getDataFolder() + File.separator + "CompleteEconomy.db";
+            service.setConnection(path);
+            service.createTables();
+        } catch (Exception e) {
+            console.sendMessage(pluginName + "Connection and Tables Error: " + e);
+        }
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome, Grandmaster " + event.getPlayer().getDisplayName() + "!");
+    }
+}
