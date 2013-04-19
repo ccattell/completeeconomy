b8e8e41509607e36c8601a370d75c9685c7d6032
eccentric devotion <eccentricdevotion@gmail.com>
2013-04-20 10:23:28 +1200

    Fix database

--- src/main/java/me/ccattell/plugins/completeeconomy/database/CEDatabase.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/database/CEDatabase.java
@@ -8,11 +8,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 /**
  *
  * @author Charlie
  */
 public class CEDatabase {
+
     private static CEDatabase instance = new CEDatabase();
     public Connection connection = null;
     public Statement statement = null;
@@ -20,21 +22,22 @@ public class CEDatabase {
     public static synchronized CEDatabase getInstance() {
         return instance;
     }
+
     public void createTables() {
         try {
             statement = connection.createStatement();
-            String queryCE = "CREATE TABLE IF NOT EXISTS CompleteEconomy (" +
-"  player_id int(11) NOT NULL AUTO_INCREMENT," +
-"  player_name text," +
-"  cash_on_hand float DEFAULT NULL," +
-"  last_login int(11) DEFAULT NULL," +
-"  PRIMARY KEY (player_id)" +
-")";
+            String queryCE = "CREATE TABLE IF NOT EXISTS CompleteEconomy ("
+                    + "  player_id int(11) NOT NULL AUTO_INCREMENT,"
+                    + "  player_name text,"
+                    + "  cash_on_hand float DEFAULT NULL,"
+                    + "  last_login int(11) DEFAULT NULL,"
+                    + "  PRIMARY KEY (player_id)"
+                    + ")";
             statement.executeUpdate(queryCE);
         } catch (SQLException e) {
-            
         }
     }
+
     public void setConnection(String path) throws Exception {
         Class.forName("org.sqlite.JDBC");
         connection = DriverManager.getConnection("jdbc:sqlite:" + path);
