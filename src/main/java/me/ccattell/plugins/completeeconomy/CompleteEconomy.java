b8debf0bb2146d5fa73f88c28464a45a678be897
Tamewolf <firehawks_general@yahoo.com>
2013-04-20 02:56:23 -0400

    Added configurable update notification
    Modified a version of an update checker I use in my plugins for use
    within this plugin.  Still some areas that need to be implemented,
    however it works just the way it is.

--- src/main/java/me/ccattell/plugins/completeeconomy/CompleteEconomy.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/CompleteEconomy.java
@@ -1,6 +1,7 @@
 package me.ccattell.plugins.completeeconomy;
 
 import java.io.File;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,6 +9,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import me.ccattell.plugins.completeeconomy.database.CEDatabase;
 
 public class CompleteEconomy extends JavaPlugin implements Listener {
@@ -19,6 +21,9 @@ public class CompleteEconomy extends JavaPlugin implements Listener {
     public String pluginName;
     public String dbtype;
 
+    //Needs to be implemented into a LoginListener
+    private UpdateChecker updateChecker;
+    
     @Override
     public void onDisable() {
         // TODO: Place any custom disable code here.
@@ -43,12 +48,23 @@ public class CompleteEconomy extends JavaPlugin implements Listener {
         } catch (Exception e) {
             console.sendMessage(pluginName + "Connection and Tables Error: " + e);
         }
+        
+        /*
+         * Gathers info from the Bukkit page, and when enabling, if the config has enabled the UpdateMessage and
+         * there is a newer file than the one they have, send a message to the console.
+        */
+        updateChecker = new UpdateChecker(this, "http://dev.bukkit.org/server-mods/complete-economy/files.rss");
+        if(updateChecker.updateNeeded()){
+        	getServer().getLogger().warning(updateChecker.getUpdateString());
+        }
+        
         service.createTables();
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+    	//Suggest making the "Grandmaster" part configurable/linked with a prefix on a permissions (probably would require Vault)
         event.getPlayer().sendMessage("Welcome, Grandmaster " + event.getPlayer().getDisplayName() + "!");
     }
 }
