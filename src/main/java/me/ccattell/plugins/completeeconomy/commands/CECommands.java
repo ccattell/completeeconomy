3bed79565082a08605d19aec38147f48ed5540e6
eccentric devotion <eccentricdevotion@gmail.com>
2013-04-23 08:56:54 +1200

    Cast sender to player, else supply name

--- src/main/java/me/ccattell/plugins/completeeconomy/commands/CECommands.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/commands/CECommands.java
@@ -5,6 +5,7 @@ import me.ccattell.plugins.completeeconomy.database.CEMainResultSet;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  *
@@ -20,7 +21,21 @@ public class CECommands implements CommandExecutor {
             return true;
         }
         if (cmd.getName().equalsIgnoreCase("cash")) {
-            String name = sender.getName();
+            String name;
+            Player player = null;
+            if (sender instanceof Player) {
+                player = (Player) sender;
+            }
+            if (player != null) {
+                name = player.getName();
+            } else {
+                // command run from console so will need to supply a player name
+                if (args.length < 1) {
+                    sender.sendMessage("You must supply a player name");
+                    return false;
+                }
+                name = args[0];
+            }
             HashMap<String, Object> where = new HashMap<String, Object>();
             where.put("player_name", name);
             CEMainResultSet rsm = new CEMainResultSet(where);
@@ -29,10 +44,10 @@ public class CECommands implements CommandExecutor {
                 // found a record so load data
                 c = rsm.getCash();
                 // do something with it
-            }else{
+            } else {
                 c = 0;
             }
-            sender.sendMessage(name + "'s Cash Balance: " + c );
+            sender.sendMessage(name + "'s Cash Balance: " + c);
             return true;
         }
         if (cmd.getName().equalsIgnoreCase("pay")) {
