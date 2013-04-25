2be6723e2fd846a061a35fa0afb8b4ddb6cd28da
eccentric devotion <eccentricdevotion@gmail.com>
2013-04-25 18:38:54 +1200

    Calculate and add interest

--- src/main/java/me/ccattell/plugins/completeeconomy/utilities/GiveInterest.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/utilities/GiveInterest.java
@@ -1,8 +1,22 @@
 package me.ccattell.plugins.completeeconomy.utilities;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import me.ccattell.plugins.completeeconomy.CompleteEconomy;
+import me.ccattell.plugins.completeeconomy.database.CEQueryFactory;
+import org.bukkit.entity.Player;
 
 public class GiveInterest {
+
+    boolean enabled = CompleteEconomy.plugin.getConfig().getBoolean("System.Interest.Enabled");
+    final boolean online = CompleteEconomy.plugin.getConfig().getBoolean("System.Interest.Online");
+    boolean announce = CompleteEconomy.plugin.getConfig().getBoolean("System.Interest.Announce");
+    long interval = CompleteEconomy.plugin.getConfig().getInt("System.Interest.Interval") * 20;
+    float cutoff = CompleteEconomy.plugin.getConfig().getInt("System.Interest.Amount.Cutoff");
+    float percent = CompleteEconomy.plugin.getConfig().getInt("System.Interest.Amount.Percent");
+    CEQueryFactory qf = new CEQueryFactory();
 
     // add class constructor
     public void GiveInterest() {
@@ -10,19 +24,40 @@ public class GiveInterest {
 
     public void interest() {
 
-        boolean enabled = CompleteEconomy.plugin.getConfig().getBoolean("System.Interest.Enabled");
-        boolean online = CompleteEconomy.plugin.getConfig().getBoolean("System.Interest.Online");
-        boolean announce = CompleteEconomy.plugin.getConfig().getBoolean("System.Announce.Enabled");
-        long interval = CompleteEconomy.plugin.getConfig().getInt("System.Interval.Seconds") * 20;
-        float cutoff = CompleteEconomy.plugin.getConfig().getInt("System.Amount.Cutoff");
-        float percent = CompleteEconomy.plugin.getConfig().getInt("System.Amount.Percent");
+        if (enabled) {
+            CompleteEconomy.plugin.getServer().getScheduler().scheduleSyncRepeatingTask(CompleteEconomy.plugin, new Runnable() {
+                @Override
+                public void run() {
+                    CompleteEconomy.plugin.getServer().getConsoleSender().sendMessage("Give Interest");
+                    // do stuff
+                    doInterest();
+                }
+            }, 60L, interval);
+        }
+    }
 
-        CompleteEconomy.plugin.getServer().getScheduler().scheduleSyncRepeatingTask(CompleteEconomy.plugin, new Runnable() {
-            @Override
-            public void run() {
-                CompleteEconomy.plugin.getServer().getConsoleSender().sendMessage("Give Interest");
+    private void doInterest() {
 
+        List<Player> online_players = Arrays.asList(CompleteEconomy.plugin.getServer().getOnlinePlayers());
+        HashMap<Player, Float> db_players = qf.getPlayers();
+        for (Map.Entry<Player, Float> entry : db_players.entrySet()) {
+            if ((online && online_players.contains(entry.getKey())) || !online) {
+                // calculate interest
+                if (entry.getValue() >= cutoff) {
+                    float credit = toCents((entry.getValue() / 100) * percent);
+                    qf.alterBalance("bank", entry.getKey().getName(), credit);
+                    if (announce) {
+                        String s = new CEMajorMinor().getFormat(credit);
+                        entry.getKey().sendMessage("You recieved " + s + "in interest on your savings!");
+                    }
+                }
             }
-        }, 60L, interval);
+        }
+    }
+
+    public static float toCents(float value) {
+        value = value * 100;
+        long tmp = Math.round(value);
+        return (float) tmp / 100;
     }
 }
