722d77684c5afd411874f0d513338017556f27f0
eccentric devotion <eccentricdevotion@gmail.com>
2013-04-25 19:48:18 +1200

    Set defaults for fields because it won't add to null

--- src/main/java/me/ccattell/plugins/completeeconomy/database/CEInitSQLite.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/database/CEInitSQLite.java
@@ -21,9 +21,9 @@ public class CEInitSQLite {
     public void initSQLite() {
         try {
             statement = connection.createStatement();
-            String queryCE = "CREATE TABLE IF NOT EXISTS CEMain (player_name TEXT PRIMARY KEY COLLATE NOCASE,  cash REAL DEFAULT NULL,  bank REAL DEFAULT NULL,  xp REAL DEFAULT NULL,  last_login INTEGER DEFAULT NULL)";
+            String queryCE = "CREATE TABLE IF NOT EXISTS CEMain (player_name TEXT PRIMARY KEY COLLATE NOCASE, cash REAL DEFAULT 0.0, bank REAL DEFAULT 0.0, xp REAL DEFAULT 0.0, last_login INTEGER DEFAULT 0)";
             statement.executeUpdate(queryCE);
-            String queryJobs = "CREATE TABLE IF NOT EXISTS CEJobs (player_name TEXT PRIMARY KEY COLLATE NOCASE, job text NOT NULL, experience INTEGER DEFAULT NULL, level INTEGER DEFAULT NULL)";
+            String queryJobs = "CREATE TABLE IF NOT EXISTS CEJobs (player_name TEXT PRIMARY KEY COLLATE NOCASE, job text NOT NULL, experience INTEGER DEFAULT 0, level INTEGER DEFAULT 0)";
             statement.executeUpdate(queryJobs);
         } catch (SQLException e) {
             System.out.println(CompleteEconomy.plugin.pluginName + "Could not create SQLite tables: " + e.getMessage());
