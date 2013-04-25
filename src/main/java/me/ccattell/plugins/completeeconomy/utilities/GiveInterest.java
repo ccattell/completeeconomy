0d8d1127863d745e2f40291ee70e92b9e57627d0
eccentric devotion <eccentricdevotion@gmail.com>
2013-04-25 15:52:17 +1200

    Add class constructor, get Boolean values

--- src/main/java/me/ccattell/plugins/completeeconomy/utilities/GiveInterest.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/utilities/GiveInterest.java
@@ -1,18 +1,18 @@
 package me.ccattell.plugins.completeeconomy.utilities;
 
 import me.ccattell.plugins.completeeconomy.CompleteEconomy;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
- 
+
 public class GiveInterest {
-    public void interest(){
-        String enabled = CompleteEconomy.plugin.getConfig().getString("System.Interest.Enabled");
-        String online = CompleteEconomy.plugin.getConfig().getString("System.Interest.Online");
-        String announce = CompleteEconomy.plugin.getConfig().getString("System.Announce.Enabled");
+
+    // add class constructor
+    public void GiveInterest() {
+    }
+
+    public void interest() {
+
+        boolean enabled = CompleteEconomy.plugin.getConfig().getBoolean("System.Interest.Enabled");
+        boolean online = CompleteEconomy.plugin.getConfig().getBoolean("System.Interest.Online");
+        boolean announce = CompleteEconomy.plugin.getConfig().getBoolean("System.Announce.Enabled");
         long interval = CompleteEconomy.plugin.getConfig().getInt("System.Interval.Seconds") * 20;
         float cutoff = CompleteEconomy.plugin.getConfig().getInt("System.Amount.Cutoff");
         float percent = CompleteEconomy.plugin.getConfig().getInt("System.Amount.Percent");
@@ -21,7 +21,7 @@ public class GiveInterest {
             @Override
             public void run() {
                 CompleteEconomy.plugin.getServer().getConsoleSender().sendMessage("Give Interest");
-                
+
             }
         }, 60L, interval);
     }
