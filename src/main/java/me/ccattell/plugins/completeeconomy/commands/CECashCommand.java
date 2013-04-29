66f851e30fea6f5f222d36663b2d9fd4a990f3ca
Charles Cattell <ccattell@mymsmtech.com>
2013-04-28 19:13:28 -0500

    added permissions to the other commands

--- src/main/java/me/ccattell/plugins/completeeconomy/commands/CECashCommand.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/commands/CECashCommand.java
@@ -18,41 +18,46 @@ public class CECashCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (cmd.getName().equalsIgnoreCase("cash")) {
-            String name = "";
-            Player player = null;
-            if (sender instanceof Player) {
-                player = (Player) sender;
-            }
-            boolean name_supplied = false; // use a boolean rather than comparing strings
-            if (args.length > 0) {
-                if (!sender.hasPermission("ce.admin")) { // console always has permission
-                    sender.sendMessage("You don't have permission to get another player's balance!");
-                    return true;
-                }
-                // player name supplied
-                name = args[0];
-                name_supplied = true;
-            } else if (player != null) {
-                name = player.getName();
-            }
-            if (!name.isEmpty()) { // name could potentially still be empty, so check
-                HashMap<String, Object> where = new HashMap<String, Object>();
-                where.put("player_name", name);
-                CEMainResultSet rsm = new CEMainResultSet(where);
-                float c;
-                if (rsm.resultSet()) {
-                    // found a record so load data
-                    c = rsm.getCash();
-                    String s = new CEMajorMinor().getFormat(c);
-                    String which = (name_supplied) ? name + "'s" : "Your";
-                    sender.sendMessage(which + " cash balance: " + s);
-                } else {
-                    // player does not exist in the CEMain table
-                    sender.sendMessage(name + " does not exist in the CE database! Check your spelling.");
-                }
+            if (!sender.hasPermission("ce.cash")) {
+                sender.sendMessage("You don't have permission to use that command!");
                 return true;
-            } else {
-                sender.sendMessage("You must supply a player name");
+            }else{
+                String name = "";
+                Player player = null;
+                if (sender instanceof Player) {
+                    player = (Player) sender;
+                }
+                boolean name_supplied = false; // use a boolean rather than comparing strings
+                if (args.length > 0) {
+                    if (!sender.hasPermission("ce.admin")) { // console always has permission
+                        sender.sendMessage("You don't have permission to get another player's balance!");
+                        return true;
+                    }
+                    // player name supplied
+                    name = args[0];
+                    name_supplied = true;
+                } else if (player != null) {
+                    name = player.getName();
+                }
+                if (!name.isEmpty()) { // name could potentially still be empty, so check
+                    HashMap<String, Object> where = new HashMap<String, Object>();
+                    where.put("player_name", name);
+                    CEMainResultSet rsm = new CEMainResultSet(where);
+                    float c;
+                    if (rsm.resultSet()) {
+                        // found a record so load data
+                        c = rsm.getCash();
+                        String s = new CEMajorMinor().getFormat(c);
+                        String which = (name_supplied) ? name + "'s" : "Your";
+                        sender.sendMessage(which + " cash balance: " + s);
+                    } else {
+                        // player does not exist in the CEMain table
+                        sender.sendMessage(name + " does not exist in the CE database! Check your spelling.");
+                    }
+                    return true;
+                } else {
+                    sender.sendMessage("You must supply a player name");
+                }
             }
         }
         return false;
--- src/main/java/me/ccattell/plugins/completeeconomy/commands/CEPayCommand.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/commands/CEPayCommand.java
@@ -18,56 +18,61 @@ public class CEPayCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (cmd.getName().equalsIgnoreCase("pay")) {
-            String to_name;
-            String from_name;
-            float pay_amount;
-
-            if (args.length < 2) { // if args.length != 2 - incorrect number of arguments?
-                sender.sendMessage("Not enough supplied arguments");
-                return false;
-            } else if (args.length > 2) {
-                sender.sendMessage("Too many supplied arguments");
-                return false;
-            } else {
-                try { // check it's a number, if not return false
-                    pay_amount = Float.parseFloat(args[0]);
-                } catch (NumberFormatException nfe) {
-                    return false;
-                }
-                to_name = args[1];
-            }
-            from_name = sender.getName();
-            if (from_name.equalsIgnoreCase(args[1])) { // can't pay yourself
-                sender.sendMessage("You can't pay yourself!");
+            if (!sender.hasPermission("ce.pay")) {
+                sender.sendMessage("You don't have permission to use that command!");
                 return true;
-            }
-            // name shouldn't be empty cause we're checking argument length
-            HashMap<String, Object> where_from = new HashMap<String, Object>();
-            where_from.put("player_name", from_name);
-            CEMainResultSet fq = new CEMainResultSet(where_from);
-            if (fq.resultSet()) {
-                // found a record so load data
-                float c;
-                String s;
-                c = fq.getCash();
-                if (c < pay_amount) {
-                    sender.sendMessage("Not enough cash on hand to complete this transaction.");
+            }else{
+                String to_name;
+                String from_name;
+                float pay_amount;
+
+                if (args.length < 2) { // if args.length != 2 - incorrect number of arguments?
+                    sender.sendMessage("Not enough supplied arguments");
+                    return false;
+                } else if (args.length > 2) {
+                    sender.sendMessage("Too many supplied arguments");
                     return false;
                 } else {
-                    HashMap<String, Object> where_to = new HashMap<String, Object>();
-                    where_to.put("player_name", to_name);
-                    CEMainResultSet tq = new CEMainResultSet(where_to);
-                    if (tq.resultSet()) {
-//                        old_to = tq.getCash();
-                        s = new CEMajorMinor().getFormat(pay_amount);
-                        CEQueryFactory qf = new CEQueryFactory();
-                        qf.alterBalance("cash", from_name, -pay_amount);
-                        qf.alterBalance("cash", to_name, pay_amount);
-                        sender.sendMessage("You paid " + to_name + " " + s);
-                        return true;
-                    } else {
-                        sender.sendMessage(to_name + " does not exist in the CE database! Check your spelling.");
+                    try { // check it's a number, if not return false
+                        pay_amount = Float.parseFloat(args[0]);
+                    } catch (NumberFormatException nfe) {
                         return false;
+                    }
+                    to_name = args[1];
+                }
+                from_name = sender.getName();
+                if (from_name.equalsIgnoreCase(args[1])) { // can't pay yourself
+                    sender.sendMessage("You can't pay yourself!");
+                    return true;
+                }
+                // name shouldn't be empty cause we're checking argument length
+                HashMap<String, Object> where_from = new HashMap<String, Object>();
+                where_from.put("player_name", from_name);
+                CEMainResultSet fq = new CEMainResultSet(where_from);
+                if (fq.resultSet()) {
+                    // found a record so load data
+                    float c;
+                    String s;
+                    c = fq.getCash();
+                    if (c < pay_amount) {
+                        sender.sendMessage("Not enough cash on hand to complete this transaction.");
+                        return false;
+                    } else {
+                        HashMap<String, Object> where_to = new HashMap<String, Object>();
+                        where_to.put("player_name", to_name);
+                        CEMainResultSet tq = new CEMainResultSet(where_to);
+                        if (tq.resultSet()) {
+    //                        old_to = tq.getCash();
+                            s = new CEMajorMinor().getFormat(pay_amount);
+                            CEQueryFactory qf = new CEQueryFactory();
+                            qf.alterBalance("cash", from_name, -pay_amount);
+                            qf.alterBalance("cash", to_name, pay_amount);
+                            sender.sendMessage("You paid " + to_name + " " + s);
+                            return true;
+                        } else {
+                            sender.sendMessage(to_name + " does not exist in the CE database! Check your spelling.");
+                            return false;
+                        }
                     }
                 }
             }
