1eb28c1bf3e21399dba1b0b3fe8c309fd3a465f6
Charles Cattell <ccattell@mymsmtech.com>
2013-05-02 16:06:39 -0500

    better error reporting

--- src/main/java/me/ccattell/plugins/completeeconomy/database/CEInitMySQL.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/database/CEInitMySQL.java
@@ -3,7 +3,8 @@ package me.ccattell.plugins.completeeconomy.database;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import me.ccattell.plugins.completeeconomy.CompleteEconomy;
+import static me.ccattell.plugins.completeeconomy.CompleteEconomy.plugin;
+import org.bukkit.ChatColor;
 
 /**
  *
@@ -14,11 +15,13 @@ public class CEInitMySQL {
     CEDatabase service = CEDatabase.getInstance();
     public Connection connection = service.getConnection();
     public Statement statement = null;
+    public String pluginName;
 
     public void CEInitMySQL() {
     }
 
     public void initMYSQL() {
+        pluginName = ChatColor.DARK_PURPLE + "[Complete Economy]" + ChatColor.RESET + " ";
         try {
             statement = connection.createStatement();
             String queryCE = "CREATE TABLE IF NOT EXISTS CEMain (player_name text NOT NULL, cash float NOT NULL, bank float NOT NULL, xp float NOT NULL, last_login int(11) NOT NULL) CHARACTER SET utf8 COLLATE utf8_general_ci";
@@ -26,7 +29,7 @@ public class CEInitMySQL {
             String queryJobs = "CREATE TABLE IF NOT EXISTS CEJobs (player_name text NOT NULL, job text NOT NULL, experience int(11) NOT NULL, level int(11) NOT NULL, status text NOT NULL) CHARACTER SET utf8 COLLATE utf8_general_ci";
             statement.executeUpdate(queryJobs);
         } catch (SQLException e) {
-            System.out.println(CompleteEconomy.plugin.pluginName + "Could not create MySQL tables: " + e.getMessage());
+            plugin.console.sendMessage(pluginName + ChatColor.GOLD + "Could not create MySQL tables: " + e.getMessage() + ChatColor.RESET);
         }
     }
 }
