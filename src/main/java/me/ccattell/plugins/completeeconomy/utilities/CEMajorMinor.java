b13ff0d4d85533ceb46e92ecf485d5274572fda4
Charles Cattell <ccattell@mymsmtech.com>
2013-05-03 12:35:49 -0500

    changed string names to prevent conflict with strings imported from config file

--- src/main/java/me/ccattell/plugins/completeeconomy/utilities/CEMajorMinor.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/utilities/CEMajorMinor.java
@@ -27,18 +27,18 @@ public class CEMajorMinor {
 
     public String getFormat(float pay_amount) {
         String s;
-        String major;
-        String minor;
+        String major_str;
+        String minor_str;
         long major_amt;
         long minor_amt;
         double m;
         if (format.equalsIgnoreCase("combined")) {
             if (pay_amount == 1) {
-                major = majorSingle;
+                major_str = majorSingle;
             } else {
-                major = majorPlural;
+                major_str = majorPlural;
             }
-            s = pay_amount + " " + major;
+            s = pay_amount + " " + major_str;
         } else {
             s = "";
             major_amt = (long) pay_amount;
@@ -47,19 +47,19 @@ public class CEMajorMinor {
             minor_amt = (long) m;
             if (major_amt > 0) {
                 if (major_amt == 1) {
-                    major = majorSingle;
+                    major_str = majorSingle;
                 } else {
-                    major = majorPlural;
+                    major_str = majorPlural;
                 }
-                s = s + major_amt + " " + major + " ";
+                s = s + major_amt + " " + major_str + " ";
             }
             if (minor_amt > 0) {
                 if (minor_amt == 1) {
-                    minor = minorSingle;
+                    minor_str = minorSingle;
                 } else {
-                    minor = minorPlural;
+                    minor_str = minorPlural;
                 }
-                s = s + minor_amt + " " + minor;
+                s = s + minor_amt + " " + minor_str;
             }
         }
         return s;
