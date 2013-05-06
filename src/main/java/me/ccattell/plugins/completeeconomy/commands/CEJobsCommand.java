38cf8c5d0f1d5bd68d4bc723e4293d59cfa9ac0b
Charles Cattell <ccattell@mymsmtech.com>
2013-05-05 19:29:50 -0500

    a few updates to jobs commands, using case insensitive jobs search

--- src/main/java/me/ccattell/plugins/completeeconomy/commands/CEJobsCommand.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/commands/CEJobsCommand.java
@@ -61,27 +61,32 @@ public class CEJobsCommand implements CommandExecutor {
                         return true;
                     }
                     // check args[1] is in the jobs list
-                    List<String> infoList = plugin.configs.getJobConfig().getStringList("Jobs.Types." + found_job);
+                    List<String> skillList = plugin.configs.getJobConfig().getStringList("Jobs.Types." + found_job);
                     player.sendMessage(moduleName + "Job description for " + found_job + ":");
                     // loop thru list
-                    for (String info : infoList) {
+                    for (String skill : skillList) {
                         //need to go one more layer deep and get info from skillsConfig on skills found associated with this job
                         // send message
-                        player.sendMessage("    " + info);
+                        player.sendMessage("    " + skill);
                     }
                     return true;
                 } else if (args[0].equalsIgnoreCase("join") && args.length == 2) {
-                    String job = args[1].toLowerCase();
-                    // check args[1] is in the jobs list
-                    if (!jobsList.contains(job)) {
-                        player.sendMessage(moduleName + "Could not find a job with that name, use /jobs list to find out the jobs you can join!");
+                    String job = args[1];
+                    for (String jobs : jobsList) {
+                        if (jobs.equalsIgnoreCase(job)) {
+                            found_job = jobs;
+                            break;
+                        }
+                    }
+                    if(found_job.equals("")){
+                        player.sendMessage(moduleName + "Could not find a job with that name, use /jobs list to find one!");
                         return true;
                     }
                     //check to see if player already has that job
                     //if(!player has job){
-                    //    doInsert("CEJobs", player = player, job = job, status = active)
+                    //    doInsert("CEJobs", player = player, job = found_job, status = active)
                     //}else if(player has inactive job){
-                    //    doUpdate("CEJobs", status = active, level = level * ReJoinPercent, experience = 0 where player = player and job = job)
+                    //    doUpdate("CEJobs", status = active, level = level * ReJoinPercent, experience = 0 where player = player and job = found_job)
                     //}
                     return true;
                 } else if (args[0].equalsIgnoreCase("stats") && args.length == 1) {
@@ -96,16 +101,21 @@ public class CEJobsCommand implements CommandExecutor {
                     //}
                     return true;
                 } else if (args[0].equalsIgnoreCase("quit") && args.length == 2) {
-                    String job = args[1].toLowerCase();
-                    // check args[1] is in the jobs list
-                    if (!jobsList.contains(job)) {
-                        player.sendMessage(moduleName + "Could not find a job with that name, use /jobs stats to find out the jobs you have!");
+                    String job = args[1];
+                    for (String jobs : jobsList) {
+                        if (jobs.equalsIgnoreCase(job)) {
+                            found_job = jobs;
+                            break;
+                        }
+                    }
+                    if(found_job.equals("")){
+                        player.sendMessage(moduleName + "Could not find a job with that name, use /jobs list to find one!");
                         return true;
                     }
                     if(DeleteOnQuit){
-                     //   doDelete("CEJobs", player = player and job = job);
+                     //   doDelete("CEJobs", player = player and job = found_job);
                     }else{
-                     //   doUpdate("CEJobs", status = 'Inactive' where player = player and job = job);
+                     //   doUpdate("CEJobs", status = 'Inactive' where player = player and job = found_job);
                     }
                     return true;
                 } else {
