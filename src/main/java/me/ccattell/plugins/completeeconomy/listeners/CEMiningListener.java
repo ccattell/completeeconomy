36490d0807e4ce8a497a11d2f8987de98ccf080d
eccentric devotion <eccentricdevotion@gmail.com>
2013-05-16 19:02:20 +1200

    Check the skills list to see if it's a breakable block

--- src/main/java/me/ccattell/plugins/completeeconomy/listeners/CEMiningListener.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/listeners/CEMiningListener.java
@@ -2,7 +2,6 @@ package me.ccattell.plugins.completeeconomy.listeners;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import me.ccattell.plugins.completeeconomy.CompleteEconomy;
 import me.ccattell.plugins.completeeconomy.database.CEQueryFactory;
 import org.bukkit.event.EventHandler;
@@ -16,12 +15,9 @@ import org.bukkit.event.block.BlockBreakEvent;
 public class CEMiningListener implements Listener {
 
     private CompleteEconomy plugin;
-    private Set<String> blocks;
 
     public void CEMiningListener(CompleteEconomy plugin) {
         this.plugin = plugin;
-        // list is static for now, but should be determined by which blocks are attached to the mining skill
-        this.blocks = plugin.configs.skillList.getConfigurationSection("Skill.Mining.break").getKeys(false);
     }
 
     @EventHandler
@@ -29,11 +25,11 @@ public class CEMiningListener implements Listener {
         // get block
         String type = event.getBlock().getType().toString();
         // is it a mining skills block?
-        if (!blocks.contains(type)) {
+        if (!plugin.configs.skillList.contains(type + ".break")) {
             return;
         }
         // does the player have mining skills as part of their job description?
-        if (!hasMiningSkill(event.getPlayer().getName())) {
+        if (!hasBreakSkill(event.getPlayer().getName(), plugin.configs.skillList.getString(type + ".break.skill"))) {
             return;
         }
         // yes & yes, so add it to the queue
@@ -64,13 +60,13 @@ public class CEMiningListener implements Listener {
         return 1;
     }
 
-    private boolean hasMiningSkill(String p) {
+    private boolean hasBreakSkill(String p, String s) {
         // get active player jobs
         HashMap<String, String> jobs = new CEQueryFactory().getPlayerJobs(p);
         if (jobs.size() > 0) {
             // check whether a job has mining skills
             for (Map.Entry<String, String> job : jobs.entrySet()) {
-                if (plugin.configs.jobList.getStringList("Jobs." + job.getKey()).contains("Mining")) {
+                if (plugin.configs.jobList.getStringList("Jobs." + job.getKey()).contains("s")) {
                     return true;
                 }
             }
