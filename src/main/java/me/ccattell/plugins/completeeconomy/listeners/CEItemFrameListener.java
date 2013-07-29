c0f83a30d130d9f9f1f07c0aabd6924cf37b0ba2
Charles Cattell <ccattell@mymsmtech.com>
2013-07-29 12:10:25 -0500

    started item creation script

--- src/main/java/me/ccattell/plugins/completeeconomy/listeners/CEItemFrameListener.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/listeners/CEItemFrameListener.java
@@ -1,6 +1,7 @@
 package me.ccattell.plugins.completeeconomy.listeners;
 
 import me.ccattell.plugins.completeeconomy.CompleteEconomy;
+import org.apache.commons.lang.WordUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -8,6 +9,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 
 /**
  *
@@ -30,7 +33,30 @@ public class CEItemFrameListener implements Listener {
         Player player = event.getPlayer();
         //String name = player.getName();
         if (player.isSneaking() && event.getRightClicked() instanceof ItemFrame) {
-            player.sendMessage(moduleName + "You already have a shop in edit mode, please save it first");
+            ItemFrame SellFrame = ((ItemFrame)event.getRightClicked());
+            ItemStack SellStack = SellFrame.getItem();
+            String SellItem = SellStack.getType().toString();
+            MaterialData data = SellStack.getData();
+            byte data_type = data.getData();
+            String block = (data_type > 0) ? SellItem + ":" + data_type : SellItem;
+            String Alias = plugin.configs.blockList.getString(block + ".alias");
+            double BuyPrice = plugin.configs.blockList.getDouble(block + ".buy");
+            if(Alias == null){
+                Alias = SellItem.replace('_', ' ').toLowerCase();
+                Alias = WordUtils.capitalizeFully(Alias);
+            }
+            double x = event.getRightClicked().getLocation().getX();
+            double y = event.getRightClicked().getLocation().getY();
+            double z = event.getRightClicked().getLocation().getZ();
+            double new_y = y-1;
+            float direction = event.getRightClicked().getLocation().getYaw();
+
+            //need to check to see if block at new_y is AIR, then place new sign filled with the following:
+            //player name
+            //Alias
+            //Buy: BuyPrice
+            //empty
+            player.sendMessage(moduleName + "Create a "+ Alias + " sign at " + x + ", " + new_y + ", " + z + " facing " + direction + " with a buy price of " + BuyPrice);
         }
     }
 }
