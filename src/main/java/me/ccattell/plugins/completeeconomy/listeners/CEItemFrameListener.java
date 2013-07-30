c9d7721012710518d3b00ca423e71e2756318bf2
eccentric devotion <eccentricdevotion@gmail.com>
2013-07-30 16:46:44 +1200

    Try this you dirty little sign

--- src/main/java/me/ccattell/plugins/completeeconomy/listeners/CEItemFrameListener.java
+++ src/main/java/me/ccattell/plugins/completeeconomy/listeners/CEItemFrameListener.java
@@ -6,6 +6,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -33,30 +36,57 @@ public class CEItemFrameListener implements Listener {
         Player player = event.getPlayer();
         //String name = player.getName();
         if (player.isSneaking() && event.getRightClicked() instanceof ItemFrame) {
-            ItemFrame SellFrame = ((ItemFrame)event.getRightClicked());
+            ItemFrame SellFrame = ((ItemFrame) event.getRightClicked());
             ItemStack SellStack = SellFrame.getItem();
             String SellItem = SellStack.getType().toString();
             MaterialData data = SellStack.getData();
             byte data_type = data.getData();
             String block = (data_type > 0) ? SellItem + ":" + data_type : SellItem;
             String Alias = plugin.configs.blockList.getString(block + ".alias");
-            double BuyPrice = plugin.configs.blockList.getDouble(block + ".buy");
-            if(Alias == null){
+            String BuyPrice = plugin.configs.blockList.getString(block + ".buy");
+            if (Alias == null) {
                 Alias = SellItem.replace('_', ' ').toLowerCase();
                 Alias = WordUtils.capitalizeFully(Alias);
             }
             double x = event.getRightClicked().getLocation().getX();
             double y = event.getRightClicked().getLocation().getY();
             double z = event.getRightClicked().getLocation().getZ();
-            double new_y = y-1;
-            float direction = event.getRightClicked().getLocation().getYaw();
+            double new_y = y - 1;
+//            float direction = event.getRightClicked().getLocation().getYaw();
+
+            BlockFace direction = SellFrame.getAttachedFace();
+            byte dir_data;
+            switch (direction) {
+                case NORTH:
+                    dir_data = 3;
+                    break;
+                case WEST:
+                    dir_data = 5;
+                    break;
+                case SOUTH:
+                    dir_data = 2;
+                    break;
+                default:
+                    dir_data = 4;
+                    break;
+            }
+            Block b = event.getRightClicked().getLocation().getBlock().getRelative(BlockFace.DOWN);
+            if (b.getTypeId() == 0) {
+                b.setTypeIdAndData(68, dir_data, true);
+                Sign sign = (Sign) b.getState();
+                sign.setLine(0, Alias);
+                sign.setLine(1, block);
+                sign.setLine(2, BuyPrice);
+                sign.update();
+            }
+
 
             //need to check to see if block at new_y is AIR, then place new sign filled with the following:
             //player name
             //Alias
             //Buy: BuyPrice
             //empty
-            player.sendMessage(moduleName + "Create a "+ Alias + " sign at " + x + ", " + new_y + ", " + z + " facing " + direction + " with a buy price of " + BuyPrice);
+            player.sendMessage(moduleName + "Create a " + Alias + " sign at " + x + ", " + new_y + ", " + z + " facing " + direction + " with a buy price of " + BuyPrice);
         }
     }
 }
