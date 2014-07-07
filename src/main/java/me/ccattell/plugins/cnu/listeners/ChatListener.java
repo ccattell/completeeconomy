7b65abae8458f830aca382e17caeb3ba46ed973d
Nicolas Lucero <Nicolas@Felipes-iMac.local>
2014-07-07 10:36:13 -0700

    Start of Chat Listener
    Listening to chat channels here.
    Replacing Player Chat Title with nick name here.

---
+++ src/main/java/me/ccattell/plugins/cnu/listeners/ChatListener.java
@@ -0,0 +1,22 @@
+
+package me.ccattell.plugins.cnu.listeners;
+
+import me.ccattell.plugins.cnu.CompleteNovusUtilities;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class ChatListener implements Listener{
+  CompleteNovusUtilities plugin;
+  public ChatListener(CompleteNovusUtilities plugin){
+    this.plugin = plugin;
+  }
+  @EventHandler
+  public void onPlayerChat(AsyncPlayerChatEvent e) {
+    
+  }
+}
